8cc987e7e19794e1590d4b77d296d031efd5053a
thechidubem <kingsleychidubem042@gmail.com>
2021-12-28 17:57:57 +0100

    updates code

--- src/components/StartingPage/StartingPageContent.js
+++ src/components/StartingPage/StartingPageContent.js
@@ -1,9 +1,18 @@
 import classes from './StartingPageContent.module.css';
+import download from "./download.jpeg"
+import home from "../home.jpeg"
 
 const StartingPageContent = () => {
   return (
     <section className={classes.starting}>
       <h1>Welcome on Board!</h1>
+      <h1>Explore with our Board!</h1>
+      <div>
+        <img src={download} alt="" />
+      </div>
+      <div>
+        <img src={home} alt="" />
+      </div>
     </section>
   );
 };
